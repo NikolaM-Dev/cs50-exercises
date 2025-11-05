effb9c63de41ac6d43193d6efaaf42cd88f9725e
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-05 11:17:11 -0500

    feat(exercises): Add mario_more exercise

---
+++ exercises/mario_more.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  const int MAX_HEIGHT = 8;
+  const int MIN_HEIGHT = 1;
+
+  int height = 0;
+  do {
+    height = get_int("Height: ");
+  } while (height > MAX_HEIGHT || height < MIN_HEIGHT);
+
+  for (int row = MIN_HEIGHT; row <= height; row++) {
+    for (int col = height; col >= MIN_HEIGHT; col--) {
+      if (col <= row) {
+        printf("#");
+      } else {
+        printf(" ");
+      }
+    }
+
+    printf("  ");
+
+    for (int col = MIN_HEIGHT; col <= row; col++) {
+      printf("#");
+    }
+
+    printf("\n");
+  }
+}
