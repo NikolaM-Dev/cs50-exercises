c262d4eaf519ac20a081303cca48b9b79a203a4b
Juan David Merchan Torres <juan1014166@gmail.com>
2025-12-03 15:57:42 -0500

    feat(exercises): Add volume exercise

--- exercises/volume.c
+++ exercises/volume.c
@@ -31,10 +31,7 @@ int main(int argc, char *argv[]) {
 
   // Copy header from input file to output file
   uint8_t header[HEADER_SIZE];
-  if (fread(header, HEADER_SIZE, 1, input) != 1) { // compared against the size
-    fprintf(stderr, "Invalid input file.\n");
-    return EXIT_FAILURE;
-  }
+  fread(header, HEADER_SIZE, 1, input);
   fwrite(header, HEADER_SIZE, 1, output);
 
   // Read samples from input file and write updated data to output file
